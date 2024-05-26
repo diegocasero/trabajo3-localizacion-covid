38379523049cd561ef781f7876e01ab627504547
diegocasero <diego.casero@alumnos.upm.es>
2024-05-26 12:06:07 +0200

    refactorizacion parsearFecha issue #3

--- src/main/java/com/practica/ems/covid/Utilidades.java
+++ src/main/java/com/practica/ems/covid/Utilidades.java
@@ -4,13 +4,7 @@ import com.practica.genericas.FechaHora;
 
 public class Utilidades {
     public static FechaHora parsearFecha (String fecha) {
-        int dia, mes, anio;
-        String[] valores = fecha.split("\\/");
-        dia = Integer.parseInt(valores[0]);
-        mes = Integer.parseInt(valores[1]);
-        anio = Integer.parseInt(valores[2]);
-        FechaHora fechaHora = new FechaHora(dia, mes, anio, 0, 0);
-        return fechaHora;
+        return parsearFecha(fecha, "0:0");
     }
 
     public static FechaHora parsearFecha (String fecha, String hora) {
